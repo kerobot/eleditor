88ce0e5e35945052cd4a267208214cc6211f894f
Manabu Kikuchi <16217397+kerobot@users.noreply.github.com>
2019-10-15 14:24:22 +0900

    edit open and save process

--- src/editor.js
+++ src/editor.js
@@ -1,20 +1,17 @@
 'use strict';
 
-const fs = require('fs');
 const {BrowserWindow, dialog} = require('electron').remote;
+const fs = require('fs');
 
 let inputArea = null;
 let inputTxt = null;
 let footerArea = null;
-
 let currentPath = '';
 let editor = null;
 
 window.addEventListener('DOMContentLoaded', onLoad);
 
-/**
- * Webページ読み込み時の処理
- */
+// Webページ読み込み時の処理
 function onLoad() {
   // 入力関連領域
   inputArea = document.getElementById('input_area');
@@ -22,14 +19,12 @@ function onLoad() {
   inputTxt = document.getElementById('input_txt');
   // フッター領域
   footerArea = document.getElementById('footer_fixed');
-
+  // エディタ関連
   editor = ace.edit('input_txt');
   editor.$blockScrolling = Infinity;
-
   editor.setTheme('ace/theme/twilight');
   editor.getSession().setMode('ace/mode/javascript');
-
-  // ドラッグ&ドロップ関連処理
+  // ドラッグ&ドロップ関連
   // イベントの伝搬を止めて、アプリケーションのHTMLとファイルが差し替わらないようにする
   document.addEventListener('dragover', (event) => {
     event.preventDefault();
@@ -37,7 +32,6 @@ function onLoad() {
   document.addEventListener('drop', (event) => {
     event.preventDefault();
   });
-
   // 入力部分の処理
   inputArea.addEventListener('dragover', (event) => {
     event.preventDefault();
@@ -54,35 +48,27 @@ function onLoad() {
     readFile(file.path);
   });
 
-  // 「読み込む」ボタンの制御
+  // 「読込」ボタン
   document.querySelector('#btnLoad').addEventListener('click', () => {
-    openLoadFile();
+    loadFile();
   });
-  // 「保存する」ボタンの制御
+  // 「保存」ボタン
   document.querySelector('#btnSave').addEventListener('click', () => {
-    openLoadFile();
+    saveFile();
   });
-};
+}
 
-/**
- * ファイルを開きます。
- */
-function openLoadFile() {
+// ファイルの読み込み
+function loadFile() {
   const win = BrowserWindow.getFocusedWindow();
-
+  // ファイルを開くダイアログ
   dialog.showOpenDialog(
     win,
-    // どんなダイアログを出すかを指定するプロパティ
     {
       properties: ['openFile'],
-      filters: [
-        {
-          name: 'Documents',
-          extensions: ['txt', 'text', 'html', 'js']
-        }
-      ]
+      filters: [{name: 'Documents', extensions: ['txt', 'text', 'html', 'js']}]
     },
-    // [ファイル選択]ダイアログが閉じられた後のコールバック関数
+    // ダイアログが閉じられた後のコールバック関数
     (fileNames) => {
       if (fileNames) {
         readFile(fileNames[0]);
@@ -90,41 +76,35 @@ function openLoadFile() {
     });
 }
 
-/**
- * テキストを読み込み、テキストを入力エリアに設定します。
- */
+// テキストを読み込み、テキストを入力エリアに設定
 function readFile(path) {
   currentPath = path;
   fs.readFile(path, (error, text) => {
-    if (error != null) {
+    if (error !== null) {
       alert('error : ' + error);
       return;
     }
-    // フッター部分に読み込み先のパスを設定する
+    // フッター部分に読み込み先のパスを設定
     footerArea.innerHTML = path;
-    // テキスト入力エリアに設定する
+    // テキスト入力エリアに設定
     editor.setValue(text.toString(), -1);
   });
 }
 
-/**
- * ファイルを保存します。
- */
+// ファイルの保存
 function saveFile() {
-
-  //　初期の入力エリアに設定されたテキストを保存しようとしたときは新規ファイルを作成する
+  //　初期の入力エリアに設定されたテキストを保存しようとしたときは新規ファイルを作成
   if (currentPath === '') {
     saveNewFile();
     return;
   }
-
   const win = BrowserWindow.getFocusedWindow();
-
+  // ファイルの上書き保存を確認
   dialog.showMessageBox(win, {
-      title: 'ファイルの上書き保存を行います。',
+      title: 'ファイル保存',
       type: 'info',
       buttons: ['OK', 'Cancel'],
-      detail: '本当に保存しますか？'
+      detail: 'ファイルを上書き保存します。よろしいですか？'
     },
     // メッセージボックスが閉じられた後のコールバック関数
     (response) => {
@@ -137,36 +117,26 @@ function saveFile() {
   );
 }
 
-/**
- * ファイルを書き込みます。
- */
+// テキストをファイルとして保存
 function writeFile(path, data) {
   fs.writeFile(path, data, (error) => {
-    if (error != null) {
+    if (error !== null) {
       alert('error : ' + error);
     }
   });
 }
 
-/**
- * 新規ファイルを保存します。
- */
+// 新規ファイルを保存
 function saveNewFile() {
-
   const win = BrowserWindow.getFocusedWindow();
+  // ファイルを保存するダイアログ
   dialog.showSaveDialog(
     win,
-    // どんなダイアログを出すかを指定するプロパティ
     {
-      properties: ['openFile'],
-      filters: [
-        {
-          name: 'Documents',
-          extensions: ['txt', 'text', 'html', 'js']
-        }
-      ]
+      properties: ['saveFile'],
+      filters: [{name: 'Documents', extensions: ['txt', 'text', 'html', 'js']}]
     },
-    // セーブ用ダイアログが閉じられた後のコールバック関数
+    // ダイアログが閉じられた後のコールバック関数
     (fileName) => {
       if (fileName) {
         const data = editor.getValue();
--- src/main.js
+++ src/main.js
@@ -10,25 +10,24 @@ const url = require('url');
 let mainWindow;
 
 function createWindow() {
-  // メインウィンドウを作成します
+  // メインウィンドウを作成
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       nodeIntegration: true
     }
-  })
+  });
 
-  // メインウィンドウに表示するURLを指定します
-  // （今回はmain.jsと同じディレクトリのindex.html）
+  // メインウィンドウに表示するURL(index.html)
   mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
+    pathname: path.join(app.getAppPath(), 'index.html'),
     protocol: 'file:',
     slashes: true
   }));
 
   // デベロッパーツールの起動
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 
   // メインウィンドウが閉じられたときの処理
   mainWindow.on('closed', () => {
@@ -36,20 +35,20 @@ function createWindow() {
   });
 }
 
-//  初期化が完了した時の処理
+// 初期化が完了した時の処理
 app.on('ready', createWindow);
 
 // 全てのウィンドウが閉じたときの処理
 app.on('window-all-closed', () => {
-  // macOSのとき以外はアプリを終了させます
+  // macOSのとき以外はアプリを終了
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
-// アプリケーションがアクティブになった時の処理(Macだと、Dockがクリックされた時）
+// アプリケーションがアクティブになった時の処理(MacはDockがクリックされた時）
 app.on('activate', () => {
-  /// メインウィンドウが消えている場合は再度メインウィンドウを作成する
+  // メインウィンドウが消えている場合は再度メインウィンドウを作成
   if (mainWindow === null) {
     createWindow();
   }
